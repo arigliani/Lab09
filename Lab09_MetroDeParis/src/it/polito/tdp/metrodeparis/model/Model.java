74b311ddeb8b3569f957c49d50beac2c9de75b80
gianni arigliani <gianni arigliani@192.168.1.12>
2017-05-18 10:43:08 +0200

    problema getPathEdgeList
    perche non mi chiama getSource sull' arco e della lista fatta con
    getPathEdgeList?

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
@@ -1,25 +1,25 @@
 package it.polito.tdp.metrodeparis.model;
 
 import java.util.ArrayList;
+//qual' è la differneza se simette come arco defoult oppure link??
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.stream.Location;
 
 import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.graph.WeightedMultigraph;
 
-import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metrodeparis.dao.MetroDAO;
 
 
 public class Model {
-	private SimpleWeightedGraph<Fermata, DefaultWeightedEdge> graph;
+	private WeightedMultigraph<Fermata, DefaultWeightedEdge> graph;
 	private List<Fermata> fermate;
-	private Map<Fermata, Fermata> alberoVisite;
 	private MetroDAO dao= new MetroDAO();
 	
 	
@@ -33,26 +33,42 @@ public class Model {
 	}
 
 
-	public SimpleWeightedGraph<Fermata, DefaultWeightedEdge> getGraph() {
+	public WeightedMultigraph<Fermata, DefaultWeightedEdge> getGraph() {
 		
 			this.creaGrafo();
 		
 		return this.graph;
 	}
+	
+	
+	public List<Fermata> getPercorso(Fermata a, Fermata b){
+		DijkstraShortestPath<Fermata, DefaultWeightedEdge> percorso= new DijkstraShortestPath<Fermata, DefaultWeightedEdge>(graph,a,b);
+		List<DefaultWeightedEdge> listaMinomo= percorso.getPathEdgeList(); //ritorna una lista di archi
+		for(DefaultWeightedEdge e: listaMinomo){
+			g
+		}
+		if(listaMinomo==null){
+			System.out.println("non esiste");
+		}else{
+			System.out.println(percorso.toString());
+
+		}
+		
+		return null;
+	}
 
 
 	private void creaGrafo() {
-		this.graph= new SimpleWeightedGraph<Fermata, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		this.graph= new WeightedMultigraph<Fermata, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 		Graphs.addAllVertices( graph, this.getFermate());
 		 DefaultWeightedEdge e;
 		for(FermateAdiacenti fa: dao.listStazioniAdiacenti()){	
 		
 		  double tempo= calcolaTempo(fa);
 		
-		  System.out.println(""+fa.getF1().getNome()+"- "+fa.getF2().getNome());
 		  e=graph.addEdge(fa.getF1(), fa.getF2());
 		   
-			graph.setEdgeWeight(e,tempo );
+			graph.setEdgeWeight(e,tempo);
 			
 		}
 		
@@ -74,7 +90,7 @@ public class Model {
 	}
 
 
-	public double distance (double lat_a, double lng_a, double lat_b, double lng_b ) 
+	private double distance (double lat_a, double lng_a, double lat_b, double lng_b ) 
 	{
 	    double earthRadius = 3958.75;
 	    double latDiff = Math.toRadians(lat_b-lat_a);
@@ -91,10 +107,7 @@ public class Model {
 	}
 
 
-	private Collection creaArchi() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 
 
--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/TestModel.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/TestModel.java
@@ -1,5 +1,8 @@
 package it.polito.tdp.metrodeparis.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
@@ -7,6 +10,11 @@ public class TestModel {
 		Model model = new Model();
 		System.out.println(""+model.getFermate());
 		System.out.println(""+model.getGraph());
+		
+		List<Fermata> list= model.getFermate();
+		
+		
+		model.getPercorso(list.get(0),list.get(4));
 	}
 
 }
